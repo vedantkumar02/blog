0a88f908ee8921114fb0f6eb4ef3eb953df8850e
Vedant Kumar <vedantkumar02@gmail.com>
2024-10-09 12:12:38 +0530

    Refactor store.js to add authSlice to the reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./authSlice";
 
 const store = configureStore({
   reducer: {
     // Add your reducers here
+    auth: authSlice,
   },
 });
 

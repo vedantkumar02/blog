4d86aa8dbc08c37249cb9f607268cc245d60c2d3
Vedant Kumar <vedantkumar02@gmail.com>
2024-10-09 12:10:17 +0530

    Refactor authentication slice and add Redux store configuration

---
+++ src/store/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  status: false,
+  userData: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.status = true;
+      state.userData = action.payload;
+    },
+    logout: (state) => {
+      state.status = false;
+      state.userData = null;
+    },
+  },
+});
+
+export {login, logout} from authSlice.reducer
+export default authSlice.reducer;

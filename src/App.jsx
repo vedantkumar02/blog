49ba614cbdc9d837a663fefa0e0038c43d6f4b2a
Vedant Kumar <vedantkumar02@gmail.com>
2024-10-09 12:56:05 +0530

    Refactor App.jsx to add authentication logic using Redux store and Appwrite SDK

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,39 @@
+import { login, logout } from "./store/authSlice";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { authService } from "./appwrite/auth";
+import { authSlice } from "./store/authSlice";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const dispatch = useDispatch();
+  const [loader, setLoader] = useState(true);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((user) => {
+        if (user) {
+          dispatch(login(user));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoader(false);
+      });
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    </div>
+  ) : null;
 }
 
 export default App;

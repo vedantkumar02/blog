f67e2d7e5f1342a0797e3448eb3887978f8eb1c0
Vedant Kumar <vedantkumar02@gmail.com>
2024-10-10 12:36:37 +0530

    Refactor PostCard component to add link functionality and improve styling

---
+++ src/component/PostCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import storageService from "../appwrite/storage";
+
+export default function PostCard({ $id, title, featuredImage }) {
+  return (
+    <Link to={`/post/${$id}`}>
+      <div className="w-full bg-gray-100 rounded-xl p-4">
+        <div className="w-full justify-center mb-4">
+          <img
+            src={storageService.getFilePreview(featuredImage)}
+            alt={title}
+            className="rounded-xl"
+          />
+        </div>
+        <h2 className="text-xl font-bold">{title}</h2>
+      </div>
+    </Link>
+  );
+}

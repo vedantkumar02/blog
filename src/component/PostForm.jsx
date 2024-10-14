4e18e8f29c917a198502ead3a12dc0c7b268f50f
Vedant Kumar <vedantkumar02@gmail.com>
2024-10-14 21:48:41 +0530

    Refactor About component and add PostForm component

--- src/component/PostForm.jsx
+++ src/component/PostForm.jsx
@@ -1,7 +1,133 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import RTE from "./RTE";
+import databaseService from "../appwrite/database";
+import storageService from "../appwrite/storage";
+import { useEffect } from "react";
+import { useCallback } from "react";
+import { useSelector } from "react-redux";
 
-function PostForm() {
-  return <div>PostForm</div>;
+function PostForm({ post }) {
+  const { register, handleSubmit, watch, setValue, getValues, control } =
+    useForm({
+      defaultValues: {
+        title: post?.title || "",
+        slug: post?.slug || "",
+        content: post?.content || "",
+        status: post?.status || "active",
+      },
+    });
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.auth.userData);
+  const submit = async (data) => {
+    if (post) {
+      data.image[0] ? storageService.uploadFile(data.image[0]) : null;
+      if (file) {
+        storageService.deleteFile(post.featuredImage);
+      }
+
+      const dbPost = await databaseService.editArticle(post.$id, {
+        ...data,
+        featuredImage: file ? file.$id : undefined,
+      });
+
+      if (dbPost) {
+        navigate("/");
+      }
+    } else {
+      const file = await storageService.uploadFile(data.image[0]);
+      if (file) {
+        const fileId = file.$id;
+        const dbPost = await databaseService.createArticle({
+          ...data,
+          featuredImage: fileId,
+          userId: userData.$id,
+        });
+        if (dbPost) {
+          navigate(`/post/${dbPost.$id}`);
+        }
+      }
+    }
+  };
+  const slugTransform = useCallback((value) => {
+    if (value && typeof value === "string") {
+      return value.trim().toLowerCase().replace(/\s+/g, "-");
+    }
+    return "";
+  }, []);
+  useEffect(() => {
+    const subscription = watch((value, { name }) => {
+      if (name === "title") {
+        setValue("slug", slugTransform(value), { shouldValidate: true });
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, [watch, slugTransform, setValue]);
+
+  return (
+    <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+      <div className="w-2/3 px-2">
+        <Input
+          label="Title :"
+          placeholder="Title"
+          className="mb-4"
+          {...register("title", { required: true })}
+        />
+        <Input
+          label="Slug :"
+          placeholder="Slug"
+          className="mb-4"
+          {...register("slug", { required: true })}
+          onInput={(e) => {
+            setValue("slug", slugTransform(e.currentTarget.value), {
+              shouldValidate: true,
+            });
+          }}
+        />
+        <RTE
+          label="Content :"
+          name="content"
+          control={control}
+          defaultValue={getValues("content")}
+        />
+      </div>
+      <div className="w-1/3 px-2">
+        <Input
+          label="Featured Image :"
+          type="file"
+          className="mb-4"
+          accept="image/png, image/jpg, image/jpeg, image/gif"
+          {...register("image", { required: !post })}
+        />
+        {post && (
+          <div className="w-full mb-4">
+            <img
+              src={appwriteService.getFilePreview(post.featuredImage)}
+              alt={post.title}
+              className="rounded-lg"
+            />
+          </div>
+        )}
+        <Select
+          options={["active", "inactive"]}
+          label="Status"
+          className="mb-4"
+          {...register("status", { required: true })}
+        />
+        <Button
+          type="submit"
+          bgColor={post ? "bg-green-500" : undefined}
+          className="w-full">
+          {post ? "Update" : "Submit"}
+        </Button>
+      </div>
+    </form>
+  );
 }
 
 export default PostForm;
